c05650f8bf7bf422e795147f0658cbb651ba7e58
Konstantin <kosty.98@mail.ru>
2022-02-07 17:05:26 +0300

    First commit

---
+++ clock/Controllers/ClockViewContorller.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  clock
+//
+//
+
+import UIKit
+import CoreLocation
+
+class ClockViewController: UIViewController {
+
+    let headerLabel = UILabel()
+    let switchButton = UISwitch()
+    let clockButton = UIButton()
+    let watchButton = UIButton()
+    let tableView = UITableView(frame: .zero, style: .plain)
+    
+    var location: CLLocation!
+//    var forecast: Forecast?
+    var placemark: CLPlacemark?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    func configureUI() {
+        
+        view.backgroundColor = UIColor(red: 0.89, green: 0.929, blue: 0.969, alpha: 1)
+        
+    }
+}
---
+++ second.swift
@@ -0,0 +1,5 @@
+//
+//  second.swift
+//  clock
+
+import Foundation
