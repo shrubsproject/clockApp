1cddc51e59fd37842103e406fe6800c8a4181cac
Konstantin <kosty.98@mail.ru>
2022-02-14 17:21:27 +0300

    #7 commit

--- clock/Controllers/AlarmController .swift
+++ clock/Controllers/AlarmController .swift
@@ -12,7 +12,10 @@ class alarmController: UIViewController{
     
     let saveButton = UIButton()
     let datePicker = UIDatePicker()
-   
+    var date = Date()
+    let textL = UILabel()
+    let formatter = DateFormatter()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,22 +26,48 @@ class alarmController: UIViewController{
     func configureUI() {
         view.addSubview(saveButton)
         view.addSubview(datePicker)
+        view.addSubview(textL)
         
         view.backgroundColor = UIColor(red: 0.89, green: 0.929, blue: 0.969, alpha: 1)
         
         saveButton.setImage(UIImage(named: "save"), for: .normal)
+        datePicker.preferredDatePickerStyle = UIDatePickerStyle.inline
+        
+        saveButton.addTarget(self, action: #selector(save), for: .touchUpOutside)
+    
+        datePicker.addTarget(self, action: #selector(dateChange), for: .valueChanged)
+        
+        
     }
     
     func configureLayout() {
         
         datePicker.translatesAutoresizingMaskIntoConstraints = false
         saveButton.translatesAutoresizingMaskIntoConstraints = false
+        textL.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             datePicker.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            datePicker.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            datePicker.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -150.0),
+            datePicker.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 25.0),
+            datePicker.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -25.0),
             
-        
+            saveButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            saveButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -15.0),
+          
+            textL.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            textL.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -250.0)
         ])
+    }
+    
+    @objc func save(){
+    
+    }
+    
+    @objc func dateChange() {
+        formatter.dateFormat = "dd/MM/yyyy/HH:mm:mm"
+        formatter.dateStyle = .full
+        formatter.timeStyle = .short
+        textL.text = formatter.string(from: datePicker.date)
     }
 }
